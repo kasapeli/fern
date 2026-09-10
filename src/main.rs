33447d806c1385aa53803b7dd231a48857b1dd88
kasapeli <yanratanaksambath@gmail.com>
2026-09-10 22:58:06 +0700

    main.rs: modify panic style

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,39 @@ mod utils;
 
 use flib::vga;
 
-use core::panic::PanicInfo;
+use core::{arch::asm, panic::PanicInfo};
 
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
+    unsafe {
+        core::arch::asm!("cli", options(nomem, nostack));
+    }
+
+    unsafe {
+        crate::vga::WRITER.set_color(0x1F);
+        crate::vga::WRITER.clear_screen();
+    }
+
     println!("!!! KERNEL PANIC !!!");
     println!("{}", info);
-    loop {}
+    println!("q to reboot");
+
+    loop {
+        unsafe {
+            let mut status: u8;
+            let mut scancode: u8;
+
+            asm!("in al, 0x64", out("al") status, options(nomem, nostack));
+            if (status & 0x01) != 0 {
+                asm!("in al, 0x60", out("al") scancode, options(nomem, nostack));
+
+                if scancode == 0x10 {
+                    crate::utils::builtins::reboot::exec();
+                }
+            }
+            core::hint::spin_loop();
+        }
+    }
 }
 
 #[unsafe(no_mangle)]
