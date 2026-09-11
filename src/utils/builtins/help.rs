1c63d0c28fe44df2b0798485ec743a59ac672df5
kasapeli <yanratanaksambath@gmail.com>
2026-09-11 10:30:06 +0700

    help.rs: add detailed help

--- src/utils/builtins/help.rs
+++ src/utils/builtins/help.rs
@@ -1,5 +1,57 @@
 use crate::kprintln;
 
-pub fn exec() {
-    kprintln!("version, help, clear, reboot, panic, halt, echo, ginfo");
+pub fn exec(args: &mut core::str::SplitWhitespace) {
+    match args.next() {
+        None => {
+            kprintln!("version, help, clear, reboot, panic, halt, echo, ginfo");
+            kprintln!("for verbose help, try help <command>");
+        }
+        Some("version") => {
+            kprintln!("version info");
+            kprintln!("usage: version");
+            kprintln!("displays info about fern and fsh's current version");
+        }
+        Some("help") => {
+            kprintln!("help info");
+            kprintln!("usage: (1) help, (2) help <command>");
+            kprintln!("(1) shows a list of command");
+            kprintln!("(2) shows help for a specific command");
+        }
+        Some("clear") => {
+            kprintln!("clear info");
+            kprintln!("usage: clear");
+            kprintln!("clears the screen");
+        }
+        Some("reboot") => {
+            kprintln!("reboot info");
+            kprintln!("usage: reboot");
+            kprintln!("reboots the system");
+        }
+        Some("panic") => {
+            kprintln!("panic info");
+            kprintln!("usage: panic");
+            kprintln!("makes the system panic");
+        }
+        Some("halt") => {
+            kprintln!("halt info");
+            kprintln!("usage: halt");
+            kprintln!("halts the CPU");
+        }
+        Some("echo") => {
+            kprintln!("echo info");
+            kprintln!("usage: (1) echo, (2) echo <message>");
+            kprintln!("(1) echoes a newline containing nothing");
+            kprintln!("(2) echoes a message");
+        }
+        Some("ginfo") => {
+            kprintln!("ginfo info");
+            kprintln!("usage: ginfo <section>");
+            kprintln!("prints info about a hardware section");
+        }
+        _ => {
+            kprintln!(
+                "invalid argument, try help for command list, or help <command> for verbose help"
+            );
+        }
+    }
 }
