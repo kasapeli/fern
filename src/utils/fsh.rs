0fd6b04add4e92bfb073764d7229dcbb8df68f32
kasapeli <yanratanaksambath@gmail.com>
2026-09-11 10:33:12 +0700

    fsh.rs: pass arg to help as mutable

--- src/utils/fsh.rs
+++ src/utils/fsh.rs
@@ -43,7 +43,7 @@ pub fn exec(cmd: &str) {
     };
 
     match command {
-        "help" => builtins::help::exec(parts),
+        "help" => builtins::help::exec(&mut parts),
         "version" => builtins::version::exec(),
         "panic" => builtins::panic::exec(),
         "clear" => builtins::clear::exec(),
