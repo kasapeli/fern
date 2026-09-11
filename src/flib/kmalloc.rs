c05e45ab5d7463b471be3d63b7a3a4d93b744063
kasapeli <yanratanaksambath@gmail.com>
2026-09-11 22:12:52 +0700

    kmalloc.rs: implement coalescing

--- src/flib/kmalloc.rs
+++ src/flib/kmalloc.rs
@@ -5,14 +5,10 @@ use core::sync::atomic::{AtomicBool, Ordering};
 
 struct ListNode {
     size: usize,
-    next: Option<&'static mut ListNode>,
+    next: *mut ListNode,
 }
 
 impl ListNode {
-    const fn new(size: usize) -> Self {
-        ListNode { size, next: None }
-    }
-
     fn start_addr(&self) -> usize {
         self as *const Self as usize
     }
@@ -79,7 +75,10 @@ struct FreeList {
 impl FreeList {
     const fn new() -> Self {
         Self {
-            head: ListNode::new(0),
+            head: ListNode {
+                size: 0,
+                next: ptr::null_mut(),
+            },
         }
     }
 
@@ -87,29 +86,49 @@ impl FreeList {
         assert_eq!(align_up(addr, mem::align_of::<ListNode>()), addr);
         assert!(size >= mem::size_of::<ListNode>());
 
-        let mut node = ListNode::new(size);
-        node.next = self.head.next.take();
-        let node_ptr = addr as *mut ListNode;
         unsafe {
-            node_ptr.write(node);
-            self.head.next = Some(&mut *node_ptr);
+            let head_ptr = &mut self.head as *mut ListNode;
+
+            let mut prev = head_ptr;
+            while !(*prev).next.is_null() && (*(*prev).next).start_addr() < addr {
+                prev = (*prev).next;
+            }
+            let next = (*prev).next;
+
+            let node_ptr = addr as *mut ListNode;
+            node_ptr.write(ListNode { size, next });
+            (*prev).next = node_ptr;
+
+            if !next.is_null() && (*node_ptr).end_addr() == (*next).start_addr() {
+                (*node_ptr).size += (*next).size;
+                (*node_ptr).next = (*next).next;
+            }
+
+            if prev != head_ptr && (*prev).end_addr() == (*node_ptr).start_addr() {
+                (*prev).size += (*node_ptr).size;
+                (*prev).next = (*node_ptr).next;
+            }
         }
     }
 
-    fn find_region(&mut self, size: usize, align: usize) -> Option<(&'static mut ListNode, usize)> {
-        let mut current = &mut self.head;
+    fn find_region(&mut self, size: usize, align: usize) -> Option<(*mut ListNode, usize)> {
+        unsafe {
+            let mut prev = &mut self.head as *mut ListNode;
 
-        while let Some(ref mut region) = current.next {
-            if let Ok(alloc_start) = Self::region_fits(region, size, align) {
-                let next = region.next.take();
-                let region = current.next.take().unwrap();
-                current.next = next;
-                return Some((region, alloc_start));
-            } else {
-                current = current.next.as_mut().unwrap();
+            loop {
+                let current = (*prev).next;
+                if current.is_null() {
+                    return None;
+                }
+
+                if let Ok(alloc_start) = Self::region_fits(&*current, size, align) {
+                    (*prev).next = (*current).next;
+                    return Some((current, alloc_start));
+                }
+
+                prev = current;
             }
         }
-        None
     }
 
     fn region_fits(region: &ListNode, size: usize, align: usize) -> Result<usize, ()> {
@@ -167,9 +186,10 @@ unsafe impl GlobalAlloc for Allocator {
         let (size, align) = size_align(layout);
         let mut list = self.list.lock();
 
-        if let Some((region, alloc_start)) = list.find_region(size, align) {
+        if let Some((region_ptr, alloc_start)) = list.find_region(size, align) {
+            let region_end = unsafe { (*region_ptr).end_addr() };
             let alloc_end = alloc_start + size;
-            let excess = region.end_addr() - alloc_end;
+            let excess = region_end - alloc_end;
             if excess > 0 {
                 unsafe {
                     list.add_free_region(alloc_end, excess);
