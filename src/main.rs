909fc7da14a998483f04cf46c8de6c63a59009fe
kasapeli <yanratanaksambath@gmail.com>
2026-09-11 15:32:52 +0700

    main.rs: reduce heap size to 256kb

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 #![no_main]
 #![allow(static_mut_refs)]
 
-static mut HEAP_MEM: [u8; 8 * 1024 * 1024] = [0; 8 * 1024 * 1024];
+static mut HEAP_MEM: [u8; 256 * 1024] = [0; 256 * 1024];
 
 #[global_allocator]
 static ALLOCATOR: flib::kmalloc::Allocator = flib::kmalloc::Allocator::new();
